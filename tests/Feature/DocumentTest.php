44db685897a1e4dc1dd712aadbc7ee8e8f7fa0b4
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2025-04-12 20:34:23 +0200

    Актуализировал тесты Document

--- tests/Feature/DocumentTest.php
+++ tests/Feature/DocumentTest.php
@@ -4,15 +4,26 @@ declare(strict_types=1);
 
 namespace Feature;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Platformaofd\ClientApi\Model\Document;
 use Platformaofd\ClientApi\Tests\TestCaseBase;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 
 class DocumentTest extends TestCaseBase
 {
-    public function test_deserializeSerialize(): void
+    public static function documentNamesDataProvider(): array
     {
-        $json = file_get_contents($this->samplesDirPath . '/Document.json');
+        return [
+            ['Document_01.json'],
+            ['Document_02.json'],
+            ['Document_03.json'],
+        ];
+    }
+
+    #[DataProvider('documentNamesDataProvider')]
+    public function test_deserializeSerialize(string $documentName): void
+    {
+        $json = file_get_contents($this->samplesDirPath . '/' . $documentName);
 
         $document = $this->serializer->deserialize(
             data: $json,
